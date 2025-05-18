b7899009f56f941e885443e2a5b7c40a069259fb
Narotam-Mishra <narotam21jan@gmail.com>
2025-05-19 00:22:31 +0530

    dashboard-project-added_Progress_Bar_In_User_profile_p1_3.1

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -8,7 +8,8 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
-import { BadgeCheck } from "lucide-react";
+import { Progress } from "@/components/ui/progress";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
 import React from "react";
 
 const SingleUserPage = () => {
@@ -47,10 +48,48 @@ const SingleUserPage = () => {
                   <p className="text-sm text-muted-foreground">This user has been verified by the admin.</p>
                 </HoverCardContent>
               </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Citrus size={36} className="rounded-full bg-orange-500/30 border-1 border-orange-500/50 p-2" />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Popular</h1>
+                  <p className="text-sm text-muted-foreground">This user has been popular in the community.</p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Shield size={36} className="rounded-full bg-green-500/30 border-1 border-green-500/50 p-2" />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Admin</h1>
+                  <p className="text-sm text-muted-foreground">Admin users have access to all features and can manage users.</p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Candy size={36} className="rounded-full bg-yellow-500/30 border-1 border-yellow-500/50 p-2" />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Awarded</h1>
+                  <p className="text-sm text-muted-foreground">This user has been awarded for their contributions.</p>
+                </HoverCardContent>
+              </HoverCard>
             </div>
           </div>
           {/* Information Container */}
-          <div className="bg-primary-foreground p-4 rounded-lg">Info</div>
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">User Information</h1>
+            <div className="space-y-4 mt-4">
+              <div className="flex flex-col gap-2 mb-8">
+                <p className="text-sm text-muted-foreground">Profile completion</p>
+                <Progress value={66} />
+              </div>
+            </div>
+          </div>
           {/* CardList Container */}
           <div className="bg-primary-foreground p-4 rounded-lg">
             <CardList title="Recent Transactions" />
