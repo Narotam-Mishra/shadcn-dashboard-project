1fa569c4ee6d4f1893512c6b8d30438e9cee5cde
Narotam-Mishra <narotam21jan@gmail.com>
2025-06-04 23:56:15 +0530

    shadcn-dashboard-project_added_Form_validation_using_zod_and_RHF_1.1

--- src/components/EditUser.tsx
+++ src/components/EditUser.tsx
@@ -1,16 +1,66 @@
 "use client"
 
-import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from "./ui/sheet"
-
+import { Phone } from "lucide-react";
+import {
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "./ui/sheet";
+import { z } from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+ 
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters!" })
+    .max(50),
+  email: z.string().email({ message: "Invalid email address!" }),
+  phone: z.string().min(10).max(15),
+  location: z.string().min(2),
+  role: z.enum(["admin", "user"]),
+});
 
 const EditUser = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "john.doe",
+      email: "john.doe@gmail.com",
+      phone: "+3 231 6578",
+      location:"New York, NY",
+      role:"admin"
+    },
+  })
+
   return (
     <SheetContent>
       <SheetHeader>
         <SheetTitle className="mb-4">Edit User</SheetTitle>
         <SheetDescription>
-          This action cannot be undone. This will permanently delete your
-          account and remove your data from our servers.
+          <Form {...form}>
+            <form className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public username.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </SheetDescription>
       </SheetHeader>
     </SheetContent>
